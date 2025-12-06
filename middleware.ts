09c1a50deb53fbd643513fb31944009901fe2edf
Shyam261510 <shyam261510@gmail.com>
2025-12-06 15:00:52 +0530

    add middlware for avoiding the unwanted routes so that user can not redirect

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const currentPath = req.nextUrl.pathname;
+
+  if (currentPath === "/Protein") {
+    return NextResponse.redirect(new URL("/Whey%20Performance", req.url));
+  }
+  if (currentPath === "/Multi%20Vitamin") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
